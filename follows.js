8e8fded4d60c15e97f5ee728956408de5b2e122f
mgradob <mgradob@gmail.com>
2014-09-03 20:57:31 -0600

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'a9KrKo29wXCznae6xgGhVX7sGS1Q8E809vFAKWPoFU5g4RQy', // Unique master Xively API key to be used as a default
+		defaultFeeds	= ["Atm_Humidity","Atm_Temperature","Cons_Humidity","Evapotranspiration","HM1_S1","HM1_S2","HM2_S1","HM2_S2","HM3_S1","HM3_S2","Pump_State","Radiation","Water_Flow","Wind_Speed"], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Water Automation', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
